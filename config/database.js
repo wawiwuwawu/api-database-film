c6ab02d6ca5eef91b83279b21df6ed4c4d414d00
wawiwuwawu <dedeari2005@gmail.com>
2025-04-19 15:44:08 +0700

    membuat endpoin untuk register akun pada database, password sudah melalui teknik hash, dan juga verifikasi agar data sesuai dengan ketentuan yang ada, cek email padakah sudah ada

--- config/database.js
+++ config/database.js
@@ -1,16 +1,23 @@
-const mysql = require('mysql2');
-// buat konfigurasi koneksi
-const koneksi = mysql.createConnection({
-    host: '192.168.0.131',
-    port: '3306',
-    user: 'root',
-    password: 'dedeari123',
-    database: 'database_film',
-    multipleStatements: true
+// config/database.js
+require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+const koneksi = mysql.createPool({
+  host: '192.168.0.131',
+  port: 3306,
+  user: 'root',
+  password: 'dedeari123',
+  database: 'database_film',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  multipleStatements: true
 });
+
+module.exports = koneksi;
 // koneksi database
-koneksi.connect((err) => {
-    if (err) throw err;
-    console.log('MySQL Connected...');
-});
+// koneksi.connect((err) => {
+//     if (err) throw err;
+//     console.log('MySQL Connected...');
+// });
 module.exports = koneksi;
