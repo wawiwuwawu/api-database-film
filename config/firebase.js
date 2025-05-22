33dc2ce3be5ca6539655a1e0d136483f11f49a16
wawiwuwawu <dedeari2005@gmail.com>
2025-05-23 05:48:38 +0700

    up

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,8 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../user-database-film-movie-pmo-firebase-adminsdk-fbsvc-ce92bbb9e3.json');
+// const admin = require('firebase-admin');
+// const serviceAccount = require('../user-database-film-movie-pmo-firebase-adminsdk-fbsvc-ce92bbb9e3.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
 
-module.exports = admin;
+// module.exports = admin;
